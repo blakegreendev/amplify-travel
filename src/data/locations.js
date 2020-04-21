10b001c1437e739d7b4220fcfb6886e4bde324d6
Blake Green <bgreen@intervision.com>
2020-04-21 13:44:47 -0700

    adding more places

--- src/data/locations.js
+++ src/data/locations.js
@@ -1,19 +1,39 @@
 import imgCostaRica from 'assets/images/costa_rica.jpg';
 import imgInterlaken from 'assets/images/interlaken.jpg';
 import imgSydney from 'assets/images/sydney.jpg';
+import imgBali from 'assets/images/bali.jpg';
+import imgCabo from 'assets/images/cabo.jpg';
+import imgMaui from 'assets/images/maui.jpg';
+import imgRome from 'assets/images/rome.jpg';
+import imgPamplona from 'assets/images/pamplona.jpg';
+import imgMunich from 'assets/images/munich.jpg';
+import imgIbiza from 'assets/images/ibiza.jpg';
+import imgSingapore from 'assets/images/singapore.jpg';
+import imgBrussels from 'assets/images/brussels.jpg';
+import imgPrague from 'assets/images/prague.jpg';
+import imgLondon from 'assets/images/london.jpg';
+import imgThailand from 'assets/images/kohpagnon.jpg';
+import imgCairns from 'assets/images/cairns.jpg';
+import imgTahoe from 'assets/images/laketahoe.jpg';
+import imgParis from 'assets/images/paris.jpg';
+import imgDublin from 'assets/images/dublin.jpg';
+import imgBangkok from 'assets/images/bangkok.jpg';
+import imgNice from 'assets/images/nice.jpg';
+import imgBarcelona from 'assets/images/barcelona.jpg';
+import imgAuckland from 'assets/images/auckland.jpg';
 
 
 export const locations = [
   {
-    placename: 'Costa Rica',
+    placename: 'Manuel Antonio, Costa Rica',
     date: 'August 1, 2015',
     image: imgCostaRica,
     location: {
-      lat: 38.958988,
-      lng: -77.417320
+      lat: 10.2735633,
+      lng: -84.0739102
     },
     todo: [
-      'Where we start! 🚀'
+      'Beach 🏖️'
     ]
   },
   {
@@ -33,11 +53,275 @@ export const locations = [
     date: 'January 17, 2011',
     image: imgSydney,
     location: {
-      lat: 33.8688,
+      lat: -33.8548157,
       lng: 151.2093
     },
     todo: [
       'Opera House 🇦🇺'
+    ]
+  },
+  {
+    placename: 'Maui, Hawaii',
+    date: 'May 9, 2016',
+    image: imgMaui,
+    location: {
+      lat: 20.8029568,
+      lng: -156.310683316022
+    },
+    todo: [
+      'Mahalo 🤙'
+    ]
+  },
+  {
+    placename: 'Nice, France',
+    date: 'September 19, 2016',
+    image: imgNice,
+    location: {
+      lat: 43.7009358,
+      lng: 7.2683912
+    },
+    todo: [
+      'Nice was nice!'
+    ]
+  },
+  {
+    placename: 'Bangkok, Thailand',
+    date: 'March 29, 2012',
+    image: imgBangkok,
+    location: {
+      lat: 13.7538929,
+      lng: 100.8160803
+    },
+    todo: [
+      'Took a tok-tok for a spin 🛵'
+    ]
+  },
+  {
+    placename: 'Dublin, Ireland',
+    date: 'January 17, 2011',
+    image: imgDublin,
+    location: {
+      lat: 53.3497645,
+      lng: -6.2602732
+    },
+    todo: [
+      'Irish tunes ☘️🇮🇪🍻'
+    ]
+  },
+  {
+    placename: 'London, England',
+    date: 'March 29, 2012',
+    image: imgLondon,
+    location: {
+      lat: 51.5073219,
+      lng: -0.1276474
+    },
+    todo: [
+      'Big Ben 🕰️🇬🇧'
+    ]
+  },
+  {
+    placename: 'Paris, France',
+    date: 'September 23, 2016',
+    image: imgParis,
+    location: {
+      lat: 48.8566101,
+      lng: 2.3514992
+    },
+    todo: [
+      'To the Louvre 🇫🇷'
+    ]
+  },
+  {
+    placename: 'Barcelona, Spain',
+    date: 'January 17, 2011',
+    image: imgBarcelona,
+    location: {
+      lat: 41.3828939,
+      lng: 2.1774322
+    },
+    todo: [
+      'I guess I looked like a tourist 🤷'
+    ]
+  },
+  {
+    placename: 'Rome, Italy',
+    date: 'November 13, 2016',
+    image: imgRome,
+    location: {
+      lat: 41.894802,
+      lng: 12.4853384
+    },
+    todo: [
+      'When in Rome '
+    ]
+  },
+  {
+    placename: 'Auckland, New Zealand',
+    date: 'June 2, 2012',
+    image: imgAuckland,
+    location: {
+      lat: -36.8534665,
+      lng: 174.7655514
+    },
+    todo: [
+      'Cruising to wine 🇳🇿'
+    ]
+  },
+  {
+    placename: 'Prague, Czech Republic',
+    date: 'March 29, 2012',
+    image: imgPrague,
+    location: {
+      lat: 50.0874654,
+      lng: 14.4212535
+    },
+    todo: [
+      'Hiking and beers '
+    ]
+  },
+  {
+    placename: 'Tahiti',
+    date: 'June 1, 2012',
+    image: imgTahiti,
+    location: {
+      lat: -17.68749305,
+      lng: -149.44507813202
+    },
+    todo: [
+      'Coconuts 🥥🌴'
+    ]
+  },
+  {
+    placename: 'Bali, Indonesia',
+    date: 'January 17, 2011',
+    image: imgBali,
+    location: {
+      lat: -8.340539,
+      lng: 115.091949
+    },
+    todo: [
+      'Elephant ride 🐘'
+    ]
+  },
+  {
+    placename: 'Cabo San Lucas, Mexico',
+    date: 'June 30, 2018',
+    image: imgCabo,
+    location: {
+      lat: 22.890533,
+      lng: -109.916740
+    },
+    todo: [
+      'Married my best friend 💒'
+    ]
+  },
+  {
+    placename: 'Munich, Germany',
+    date: 'October 4, 2009',
+    image: imgMunich,
+    location: {
+      lat: 48.135124,
+      lng: 11.581981
+    },
+    todo: [
+      'Oktoberfest 🍻'
+    ]
+  },
+  {
+    placename: 'Pamplona, Spain',
+    date: 'July 13, 2010',
+    image: imgPamplona,
+    location: {
+      lat: 42.812527,
+      lng: -1.645774
+    },
+    todo: [
+      'Running with the bulls 🐂'
+    ]
+  },
+  {
+    placename: 'Ibiza, Spain',
+    date: 'June 4, 2011',
+    image: imgIbiza,
+    location: {
+      lat: 39.020012,
+      lng: 1.482148
+    },
+    todo: [
+      'Party 🥳'
+    ]
+  },
+  {
+    placename: 'Singapore',
+    date: 'September 23, 2018',
+    image: imgSingapore,
+    location: {
+      lat: 1.352083,
+      lng: 103.819839
+    },
+    todo: [
+      'Gardens by the Bay 🇸🇬'
+    ]
+  },
+  {
+    placename: 'Florence, Italy',
+    date: 'September 20, 2016',
+    image: imgFlorence,
+    location: {
+      lat: 43.769562,
+      lng: 11.255814
+    },
+    todo: [
+      'Mambo Italiano'
+    ]
+  },
+  {
+    placename: 'Brussels, Belgium',
+    date: 'March 29, 2012',
+    image: imgBrussels,
+    location: {
+      lat: 50.845539,
+      lng: 4.355710
+    },
+    todo: [
+      'Belgium Waffles 🧇'
+    ]
+  },
+  {
+    placename: 'Koh Phangan, Thailand',
+    date: 'March 29, 2012',
+    image: imgThailand,
+    location: {
+      lat: 9.720270,
+      lng: 99.995743
+    },
+    todo: [
+      'Full Moon Party 🌕'
+    ]
+  },
+  {
+    placename: 'Cairns, Australia',
+    date: 'June 3, 2012',
+    image: imgCairns,
+    location: {
+      lat: -16.917895,
+      lng: 145.776793
+    },
+    todo: [
+      'Neature Walk '
+    ]
+  },
+  {
+    placename: 'Lake Tahoe, California',
+    date: 'January 30, 2016',
+    image: imgTahoe,
+    location: {
+      lat: 38.941631,
+      lng: -119.977219
+    },
+    todo: [
+      'Shreddin the gnar '
     ]
   },
 ];
